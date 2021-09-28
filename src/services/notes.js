6f2290c1dbbf2e8b1f232b331680e45a3fe27896
Amutha <amuthamuhunthan@Amuthas-iMac.local>
2021-09-28 11:02:46 +1000

    fetch data from backend node express

---
+++ src/services/notes.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/api/notes";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  const nonExisting = {
+    id: 10000,
+    content: "This note is not saved to server",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  };
+  return request.then((response) => response.data.concat(nonExisting));
+  // return request.then(response => response.data)
+};
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+// Since the names of the keys and the assigned variables are the same, we can write the object definition with more compact syntax:
+// from this to ES6 {
+//   getAll: getAll,
+//   create: create,
+//   update: update
+// }
+//  to this
+export default { getAll, create, update };
